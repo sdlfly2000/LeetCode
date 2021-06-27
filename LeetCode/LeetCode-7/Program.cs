42048eb1e0f95241b89e8fbc3fe9ef9b15457d0a
Jay Shi <25982097@qq.com>
2021-06-28 01:29:16 +0800

    7-Solution

--- LeetCode/LeetCode-7/Program.cs
+++ LeetCode/LeetCode-7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace LeetCode_7
 {
@@ -8,7 +9,7 @@ namespace LeetCode_7
         {
             var solution = new Solution();
 
-            solution.Reverse(-236);
+            solution.Reverse(int.MaxValue);
         }
     }
 
@@ -17,10 +18,55 @@ namespace LeetCode_7
         public int Reverse(int x)
         {
             var sX = x.ToString();
+            var sign = sX[0] == '-';
+            var numProcessed = sign ? sX.Remove(0, 1) : sX;
+            var rev = Reverse(numProcessed, sign);
 
-            Console.WriteLine(sX);
+            Console.WriteLine(rev);
 
             return 0;
+        }
+
+        private int Reverse(string num, bool sign)
+        {
+            var ret = new StringBuilder();
+            var i = num.Length - 1;
+
+            while(i >= 0)
+            {
+                ret.Append(num[i]);
+                i--;
+            }
+
+            return Parse(ret.ToString(), sign);
+        }
+
+        private int Parse(string num, bool sign)
+        {
+            var ret = 0;
+            for(var i = 0; i < num.Length; i++)
+            {
+                ret += (num[i] - 48) * GetMultiply(num.Length - i);
+            }
+
+            if (sign)
+            {
+                ret *= -1;
+            }
+
+            return ret;            
+        }
+
+        private int GetMultiply(int time)
+        {
+            var ret = 1;
+            while(time > 1)
+            {
+                ret *= 10;
+                time--;
+            }
+
+            return ret;
         }
     }
 }
