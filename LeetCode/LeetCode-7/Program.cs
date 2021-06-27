f3a4a8c83c7bd1e91c4c9623f6639a53561e29fd
Jay Shi <25982097@qq.com>
2021-06-27 08:46:17 +0800

    Initial Solution-7

---
+++ LeetCode/LeetCode-7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            solution.Reverse(-236);
+        }
+    }
+
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            var sX = x.ToString();
+
+            Console.WriteLine(sX);
+
+            return 0;
+        }
+    }
+}
