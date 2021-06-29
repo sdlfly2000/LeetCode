5d034605629026233184a94235fe177789cd0bca
Jay Shi <25982097@qq.com>
2021-06-30 00:43:46 +0800

    complete solution 8

--- LeetCode/LeetCode-8/Program.cs
+++ LeetCode/LeetCode-8/Program.cs
@@ -7,8 +7,15 @@ namespace LeetCode_8
     {
         static void Main(string[] args)
         {
+            // "00000-42a1234" -> 0
+            // "  -0012a42" -> -12
+            // "   -42" -> -42
+            // "   +0 123" -> 0
+            // "    -88827   5655  U" -> -88827
+            // "-5-" -> -5
+
             var solution = new Solution();
-            var result = solution.MyAtoi("9223372036854775808");
+            var result = solution.MyAtoi("  -0012a42");
 
             Console.WriteLine(result);
         }
@@ -28,6 +35,11 @@ namespace LeetCode_8
             var ret = 0L;
             var sign = num[0] == '-';
             var numPart = sign ? num.Substring(1) : num;
+
+            if(numPart.Length > 10)
+            {
+                return sign ? int.MinValue : int.MaxValue;
+            }
 
             for(var i = 0; i < numPart.Length; i++)
             {
@@ -63,57 +75,68 @@ namespace LeetCode_8
         private string Extract(string s)
         {
             var ret = new StringBuilder();
-            var signed = false;
+            var validSign = false;
             var sign = false;
             var len = s.Length;
             for(var i = 0; i < len; i++)
             {
                 if(s[i] == '-')
                 {
-                    if (signed)
+                    if (validSign)
                     {
-                        sign = false;
                         break;
                     }
 
-                    if(ret.Length > 0)
+                    if(ret.Length > 0 || (i > 0 && validSign))
                     {
-                        sign = false;
                         break;
                     }
 
                     sign = true;
-                    signed = true;
+                    validSign = true;
                 }
                 else if(s[i] == '+')
                 {
-                    if (signed)
+                    if (validSign)
                     {
-                        sign = false;
                         break;
                     }
 
                     if (ret.Length > 0)
                     {
-                        sign = false;
                         break;
                     }
 
                     sign = false;
-                    signed = true;
+                    validSign = true;
                 }
                 else if(s[i] == ' ')
                 {
-                    if (ret.Length > 0)
+                    if (ret.Length > 0 || validSign)
                     {
-                        sign = false;
                         break;
                     }
+
                     continue;
                 }
-                else if(s[i] > 47 && s[i] < 59)
+                else if(s[i] > 48 && s[i] < 59)
                 {
                     ret.Append(s[i]);
+                    validSign = true;
+                }
+                else if (s[i] == 48)
+                {
+                    validSign = true;
+                    if (i == 0 || ret.Length == 0) 
+                    {
+                        continue;
+                    }
+
+                    ret.Append(s[i]);
+                }
+                else if (s[i] == 46)
+                {
+                    break;
                 }
                 else
                 {
@@ -121,11 +144,12 @@ namespace LeetCode_8
                     {
                         sign = false;
                     }
+
                     break;                   
                 }
             }
 
-            if (sign)
+            if (sign && ret.Length > 0)
             {
                 ret.Insert(0, '-'); 
             }
