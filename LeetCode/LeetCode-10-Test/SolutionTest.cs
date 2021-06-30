c930cb9a30a1d74a8b6c3ca7cdb3be6504b1bc79
Jay Shi <25982097@qq.com>
2021-07-01 00:51:09 +0800

    Intial LeetCode 10

---
+++ LeetCode/LeetCode-10-Test/SolutionTest.cs
@@ -0,0 +1,87 @@
+using LeetCode_10;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_10_Test
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        private Solution _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new Solution();
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void SolutionTest_1()
+        {
+            // Arrange
+            var s = "aa";
+            var p = "a";
+
+            // Act
+            var output = _solution.IsMatch(s, p);
+
+            // Assert
+            Assert.IsFalse(output);
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void SolutionTest_2()
+        {
+            // Arrange
+            var s = "aa";
+            var p = "a*";
+
+            // Act
+            var output = _solution.IsMatch(s, p);
+
+            // Assert
+            Assert.IsTrue(output);
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void SolutionTest_3()
+        {
+            // Arrange
+            var s = "ab";
+            var p = ".*";
+
+            // Act
+            var output = _solution.IsMatch(s, p);
+
+            // Assert
+            Assert.IsTrue(output);
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void SolutionTest_4()
+        {
+            // Arrange
+            var s = "aab";
+            var p = "c*a*b";
+
+            // Act
+            var output = _solution.IsMatch(s, p);
+
+            // Assert
+            Assert.IsTrue(output);
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void SolutionTest_5()
+        {
+            // Arrange
+            var s = "mississippi";
+            var p = "mis*is*p*.";
+
+            // Act
+            var output = _solution.IsMatch(s, p);
+
+            // Assert
+            Assert.IsFalse(output);
+        }
+    }
+}
---
+++ LeetCode/LeetCode-10/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_10
+{
+    public class Solution
+    {
+        public bool IsMatch(string s, string p)
+        {
+            return false;
+        }
+    }
+}
