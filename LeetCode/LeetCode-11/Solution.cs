1403bea731d232e364edd1abb02c8a22fc1d478f
Jay Shi <25982097@qq.com>
2021-07-12 01:01:13 +0800

    progress solution 11

--- LeetCode/LeetCode-11/Solution.cs
+++ LeetCode/LeetCode-11/Solution.cs
@@ -8,7 +8,20 @@ namespace LeetCode_11
     {
         public int MaxArea(int[] height)
         {
+            var maxArea = 0;
 
+            for(var i = 0; i < height.Length; i++)
+            {
+                for(var j = i + 1; j < height.Length; j++)
+                {
+                    var iHeight = height[i] < height[j] ? height[i] : height[j];
+                    var iWidth = j - i;
+                    var iArea = iHeight * iWidth;
+                    maxArea = iArea > maxArea ? iArea : maxArea;
+                }
+            }
+
+            return maxArea;
         }
     }
 }
