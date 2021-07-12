7a52e57f4843e4ac0dd885130718ba2a008131f2
Jay Shi <25982097@qq.com>
2021-07-13 00:52:23 +0800

    Finish solution 12

---
+++ LeetCode/LeetCode-11/Solution2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_11
+{
+    public class Solution2
+    {
+        // Double Point Arg
+        public int MaxArea(int[] height)
+        {
+            var maxArea = 0;
+            var i = 0;
+            var j = height.Length - 1;
+            while (i < j)
+            {
+                var iHeight = height[i] < height[j] ? height[i] : height[j];
+                var iWidth = j - i;
+                var area = iHeight * iWidth;
+                maxArea = area > maxArea ? area : maxArea;
+                if(height[i] < height[j])
+                {
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+            return maxArea;
+        }
+    }
+}
---
+++ LeetCode/LeetCode-12/Solution.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_12
+{
+    public class Solution
+    {
+        public string IntToRoman(int num)
+        {
+            var roman = new StringBuilder();
+            var digit4 = num / 1000;
+            var digit3 = (num % 1000) / 100;
+            var digit2 = (num % 100) / 10;
+            var digit1 = (num % 10);
+
+            for(var i = 0; i < digit4; i++)
+            {
+                roman.Append("M");
+            }
+
+            roman.Append(MapRoman(digit3, 'C', 'D', 'M'));
+            roman.Append(MapRoman(digit2, 'X', 'L', 'C'));
+            roman.Append(MapRoman(digit1, 'I', 'V', 'X'));
+
+            return roman.ToString();
+        }
+
+        private string MapRoman(int digit, char low, char hight, char next)
+        {
+            var roman = new StringBuilder();
+            if (digit < 4)
+            {
+                for (var i = 0; i < digit; i++)
+                {
+                    roman.Append(low);
+                }
+            }
+            else if (digit == 4)
+            {
+                roman.Append(low);
+                roman.Append(hight);
+            }
+            else if (digit == 5)
+            {
+                roman.Append(hight);
+            }
+            else if (digit == 9)
+            {
+                roman.Append(low);
+                roman.Append(next);
+            }
+            else
+            {
+                roman.Append(hight);
+                for (var i = 5; i < digit; i++)
+                {
+                    roman.Append(low);
+                }
+            }
+            return roman.ToString();
+        }
+    }
+}
