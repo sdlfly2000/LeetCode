29ccb504c39cd866c532ce345dff9653615cf8b2
Jay Shi <25982097@qq.com>
2021-07-14 23:55:15 +0800

    Initial solution 14

---
+++ LeetCode/LeetCode-14/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_14
+{
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            for(var i = 0; i < strs.Length; i++)
+            {
+                var tempChar = strs[i]
+            }
+        }
+    }
+}
