620f78e1cecf2646f36de3e0ef16a873dee719b9
Jay Shi <JayShi@eurofins.com>
2021-08-26 09:49:16 +0800

    Solution 16: Double Points

---
+++ LeetCode/LeetCode-16-Test/Solution2Test.cs
@@ -0,0 +1,94 @@
+using LeetCode_16;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_16_Test
+{
+    [TestClass]
+    public class Solution2Test
+    {
+        private Solution2 _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new Solution2();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var num = new int[] { -1, 2, 1, -4 };
+            var target = 1;
+
+            var result = _solution.ThreeSumClosest(num, target);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var num = new int[] { 0, 1, 2 };
+            var target = 0;
+
+            var result = _solution.ThreeSumClosest(num, target);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var num = new int[] { -3, -2, -5, 3, -4};
+            var target = -1;
+
+            var result = _solution.ThreeSumClosest(num, target);
+
+            Assert.AreEqual(-2, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var num = new int[] { 1, -3, 3, 5, 4, 1};
+            var target = 1;
+
+            var result = _solution.ThreeSumClosest(num, target);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var num = new int[] { 0, 2, 1, -3};
+            var target = 1;
+
+            var result = _solution.ThreeSumClosest(num, target);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            var num = new int[] { 1, 2, 4, 8, 16, 32, 64, 128 };
+            var target = 82;
+
+            var result = _solution.ThreeSumClosest(num, target);
+
+            Assert.AreEqual(82, result);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            var num = new int[] { 4, 0, 5, -5, 3, 3, 0, -4, -5 };
+            var target = -2;
+
+            var result = _solution.ThreeSumClosest(num, target);
+
+            Assert.AreEqual(-2, result);
+        }
+    }
+}
---
+++ LeetCode/LeetCode-16/Solution2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_16
+{
+    public class Solution2
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+
+            var mostClosestSum = 0;
+            var mostClosestDiff = int.MaxValue;
+
+            for (var index = 0; index < nums.Length; index++)
+            {
+                var startIndex = index + 1;
+                var endIndex = nums.Length - 1;
+
+                while (startIndex < endIndex)
+                {
+                    var currentSum = nums[index] + nums[startIndex] + nums[endIndex];
+                    var currentDiff = currentSum - target;
+
+                    if (currentSum == target)
+                    {
+                        return currentSum;
+                    }
+
+                    if (Math.Abs(currentDiff) < Math.Abs(mostClosestDiff))
+                    {
+                        mostClosestSum = currentSum;
+                        mostClosestDiff = currentDiff;
+                    }
+
+                    if (currentSum <= target)
+                    {
+                        startIndex++;
+                    }
+
+                    if (currentSum > target)
+                    {
+                        endIndex--;
+                    }
+                }
+            }
+
+            return mostClosestSum;
+        }
+    }
+}
